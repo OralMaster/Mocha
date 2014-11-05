f3ab838d6013763bfdd36b3d5614cb36a5d8668d
OralMaster <lyluyaoh@gmail.com>
2014-11-04 22:35:04 -0600

    Setup

---
+++ MOCHA/MOCHA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MOCHA
+//
+//  Created by Luyao Huang on 14/11/4.
+//  Copyright (c) 2014年 GoodYellow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
